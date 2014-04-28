2588e6fb39f983aac15e4fb1cc85969ac2fffe70
kunzimariano <kunzi.mariano@gmail.com>
2014-04-28 17:10:35 -0300

    Encapsulation and renaming according to it.

--- AwsManagementTools.psm1
+++ AwsManagementTools.psm1
@@ -1,17 +1,17 @@
-﻿filter Is-Running {
+﻿filter IsRunning {
     if($_.State.Name -eq 'running') { $_ }
 }
 
-filter IsNot-Running {
+filter IsNotRunning {
     if($_.State.Name -ne 'running') { $_ }
 }
 
-filter IsNot-Stopped {
+filter IsNotStopped {
     if($_.State.Name -ne 'stopped') { $_ }
 }
 
 function Stop-AllEC2Instances {
-    (Get-EC2Instance).Instances | IsNot-Stopped |
+    (Get-EC2Instance).Instances | IsNotStopped |
     % { Stop-EC2Instance -Instance $_.InstanceId -Force }
 }
 
@@ -20,29 +20,29 @@ function Get-EC2InstancesWithFilter {
     (Get-EC2Instance -Filter @{ Name="tag:Name"; Value="$filter"}).Instances
 }
 
-function Write-RunningInfo {
+function WriteRunningInfo {
     param([parameter(Mandatory=$true,ValueFromPipeline=$true)] [object]$instance)
     process {
         Write-Host $instance.InstanceId : "<$($instance.PublicIpAddress)>" : $instance.State.Name : $instance.Tags.Value
     }
 }
 
-function Write-NotRunningInfo {
+function WriteNotRunningInfo {
     param([parameter(Mandatory=$true,ValueFromPipeline=$true)] [object]$instance)
     process {
         Write-Host $instance.InstanceId : $instance.State.Name : $instance.Tags.Value
     }
 }
 
-function Get-EC2InstancesStatus {
+function GetEC2InstancesStatus {
     param(
     [parameter(Mandatory=$true)][object[]]$instancesId,
     [parameter(Mandatory=$true)][string]$retry)
     
     $maxRetries = 20
     $instances  = (Get-EC2Instance -Instance $instancesId).Instances
-    ($running = $instances | Is-Running) | Write-RunningInfo
-    $notRunning = $instances | IsNot-Running
+    ($running = $instances | IsRunning) | WriteRunningInfo
+    $notRunning = $instances | IsNotRunning
     
     $keepGoing = ($notRunning -ne $null) -and ($notRunning.Length -gt 0) -and ($retry -lt $maxRetries)
     $stop = ($notRunning -ne $null) -and ($notRunning.Length -gt 0)
@@ -50,23 +50,25 @@ function Get-EC2InstancesStatus {
     if ($keepGoing)
     {
         Write-Host "Waiting for the next instances to start..."
-        $notRunning | Write-NotRunningInfo
+        $notRunning | WriteNotRunningInfo
         Start-Sleep -s 30
-        Get-EC2InstancesStatus $notRunning.InstanceId ($retry + 1)
+        GetEC2InstancesStatus $notRunning.InstanceId ($retry + 1)
     }
     elseif($stop)
     {
         Write-Host "After $maxRetries retries the next instances failed to go into running."
-        $notRunning | Write-NotRunningInfo
+        $notRunning | WriteNotRunningInfo
     }
 }
 
 function Start-EC2InstancesWithFilter {
     param([parameter(Mandatory=$true)] [string]$filter)
     
-    ($instances = Get-EC2InstancesWithFilter $filter | IsNot-Running ) |
+    ($instances = Get-EC2InstancesWithFilter $filter | IsNotRunning ) |
     Start-EC2Instance >> $null
     
-    Get-EC2InstancesStatus $instances.InstanceId 1
+    GetEC2InstancesStatus $instances.InstanceId 1
     Write-Host "Finished running Start-EC2InstancesWithFilter."
 }
+
+Export-ModuleMember -Function Stop-AllEC2Instances,Get-EC2InstancesWithFilter,Start-EC2InstancesWithFilter
