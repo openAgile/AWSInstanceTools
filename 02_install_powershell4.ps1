b535b93d544e516bae6d4edd75fac0aac3d3f6ff
Josh Gough <jsgough@gmail.com>
2014-05-06 17:16:39 -0400

    Prompt user for reboot

--- 02_install_powershell4.ps1
+++ 02_install_powershell4.ps1
@@ -1,4 +1,26 @@
 Set-ExecutionPolicy Unrestricted
-#installs powershell 4, reboot after that
-cinst powershell4
-Write-Host "If the install succeeds, then reboot the computer."
+if ($PSVersionTable.PSVersion.Major -lt 4) {
+  cinst powershell4
+  $title = "Reboot"
+  $message = "Would you like to reboot now to complete the Powershell 4 upgrade?"
+  
+  $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", `
+      "Reboot now."
+  
+  $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
+      "Do not reboot now."
+  
+  $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+  
+  $result = $host.ui.PromptForChoice($title, $message, $options, 0) 
+  
+  switch ($result)
+      {
+          0 { 
+            shutdown /r
+          }
+          1 { "Not rebooting."}
+      }
+} else {
+  Write-Host "You already have Powershell version 4 or higher!"
+}
