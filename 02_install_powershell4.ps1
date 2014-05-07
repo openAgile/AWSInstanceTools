3d763b6a92d5a2d83283cdf5a2127c363285e85d
Josh Gough <jsgough@gmail.com>
2014-05-07 09:57:16 -0400

    Update 02_install_powershell4.ps1

--- 02_install_powershell4.ps1
+++ 02_install_powershell4.ps1
@@ -2,7 +2,7 @@ Set-ExecutionPolicy Unrestricted
 if ($PSVersionTable.PSVersion.Major -lt 4) {
   cinst powershell4
   $title = "Reboot"
-  $message = "Sometimes a reboot is required after upgrading to Powershell 4. Would you like to reboot now to safe?"
+  $message = "Sometimes a reboot is required after upgrading to Powershell 4. Would you like to reboot now to be safe?"
   
   $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", `
       "Reboot now."
