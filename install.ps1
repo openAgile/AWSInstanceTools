6fda312d681252b468683973c265a6d2c4bce03e
Josh Gough <jsgough@gmail.com>
2014-07-23 17:46:19 -0400

    Better prompt messaging

--- install.ps1
+++ install.ps1
@@ -14,10 +14,10 @@ if ($PSVersionTable.PSVersion.Major -lt 5) {
 	Write-Host "You don't have Powershell 4. Trying to install it for you via Chocolatey..."
 	cinst powershell4
 	if ($?) {
-	  Write-Host "Powershell 4 upgrade succeeded! Please start a new Administrator Powershell prompt then run this command: (new-object net.webclient).DownloadString('https://raw.githubusercontent.com/openAgile/AWSInstanceTools/master/install_modules.ps1') | iex"		
+	  Write-Host "Powershell 4 upgrade succeeded! Please start a new Administrator Powershell prompt then run this command:`n`n(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/openAgile/AWSInstanceTools/master/install_modules.ps1') | iex`n`n"
 	} else {
 	  Write-Error "Could not install Powershell 4 with Chocolatey. Please try to install it manually."
-	  Write-Error "Once you've installed it manually, run this command from an Administrator Powershell prompt: (new-object net.webclient).DownloadString('https://raw.githubusercontent.com/openAgile/AWSInstanceTools/master/install_modules.ps1') | iex"
+	  Write-Error "Once you've installed it manually, run this command from an Administrator Powershell prompt:`n`n(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/openAgile/AWSInstanceTools/master/install_modules.ps1') | iex`n`n"
 	}
 } else {
   Write-Host "You already have Powershell 4 installed. Great! Now installing additional Chocolatey packages and Powershell modules..."
