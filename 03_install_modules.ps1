0dff71673bde34f90b8ed6125eb0fbfbdb747b91
Josh Gough <jsgough@gmail.com>
2014-07-23 17:00:30 -0400

    Initialize default credentials for AWS

--- 03_install_modules.ps1
+++ 03_install_modules.ps1
@@ -1,4 +1,8 @@
 cinst AWSTools.Powershell
+if (test-path \\files\pub\openAgile\ProviderCredentials\AmazonWebServices\InitializeDefaultCreds.ps1) {
+  write-host "Setting your default credentials for AWS..."
+  \\files\pub\openAgile\ProviderCredentials\AmazonWebServices\InitializeDefaultCreds.ps1
+}
 (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
 Import-Module psget
 Install-Module AWSInstanceTools -Global -Update
