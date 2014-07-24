c0247912523ad81e1b84e71696a6a7ecc1bd4261
Josh Gough <jsgough@gmail.com>
2014-07-24 14:25:33 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -16,12 +16,17 @@ if ($PSVersionTable.PSVersion.Major -lt 4) {
 	if ($?) {
 		Write-Host "Powershell 4 upgrade succeeded! Now installing additional Chocolatey packages and Powershell modules...`n`n"
 		powershell -Version 4.0 -NoExit -Command "(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/openAgile/AWSInstanceTools/master/install_modules.ps1') | iex"
-
+		if ($?) {
+			Write-Host "AWSInstanceTools installation complete!"
+		}
 	} else {
 	  Write-Error "Could not install Powershell 4 with Chocolatey. Please try to install it manually."
 	  Write-Error "Once you've installed it manually, run this command from an Administrator Powershell prompt:`n`n(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/openAgile/AWSInstanceTools/master/install_modules.ps1') | iex`n`n"
 	}
 } else {
-  Write-Host "You already have Powershell 4 installed. Great! Now installing additional Chocolatey packages and Powershell modules..."
-  (new-object net.webclient).DownloadString('https://raw.githubusercontent.com/openAgile/AWSInstanceTools/master/install_modules.ps1') | iex
+	Write-Host "You already have Powershell 4 installed. Great! Now installing additional Chocolatey packages and Powershell modules..."
+  	(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/openAgile/AWSInstanceTools/master/install_modules.ps1') | iex
+	if ($?) {
+		Write-Host "AWSInstanceTools installation complete!"
+	}
 }
