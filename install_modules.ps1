1fe2d0a9e384624a1ca770bfbe691c82e35857c8
Josh Gough <jsgough@gmail.com>
2014-07-24 14:24:26 -0400

    Update install_modules.ps1

--- install_modules.ps1
+++ install_modules.ps1
@@ -17,6 +17,3 @@ Import-Module psget
 Install-Module AWSInstanceTools -Global
 Import-Module AWSInstanceTools
 Update-Module AWSInstanceTools -Global
-if ($?) {
-	Write-Host "Installation finished!"
-}
