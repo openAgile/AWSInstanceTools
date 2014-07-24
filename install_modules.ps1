62d01c0078dfa808c956269afb22ec158897e1c2
Josh Gough <jsgough@gmail.com>
2014-07-24 11:27:13 -0400

    Import module with path and message for non-V1ers

--- install_modules.ps1
+++ install_modules.ps1
@@ -1,9 +1,10 @@
 cinst AWSTools.Powershell
-#Import-Module 'C:\Program Files (x86)\AWS Tools\PowerShell\AWSPowerShell\AWSPowerShell.psd1'
-Import-Module AWSPowerShell
+Import-Module 'C:\Program Files (x86)\AWS Tools\PowerShell\AWSPowerShell\AWSPowerShell.psd1'
 if (test-path \\files\pub\openAgile\ProviderCredentials\AmazonWebServices\InitializeDefaultCreds.ps1) {
   write-host "Setting your default credentials for AWS..."
   \\files\pub\openAgile\ProviderCredentials\AmazonWebServices\InitializeDefaultCreds.ps1
+} else {
+	Write-Error "You are either not on the VersionOne network or you're not a VersionOne employee! Please refer to the AWS Tools for Windows Powershell documentation for setting your default credentials to AWS."
 }
 (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
 Import-Module psget
